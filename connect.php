716277c25bf80cdf7a1f31c150057142d96f65f8
chedli <chedlidon@gmail.com>
2021-01-24 22:25:37 +0100

    update

--- connect.php
+++ connect.php
@@ -4,6 +4,6 @@ $NU="root";
 $mdp="";
 $bd="techup";
 $idcnx=mysqli_connect($SBD,$NU,$mdp) or die ("impossible de cnct");
-mysqli_select_db($idcnx,$bd) or die ("echec de cnct");
+mysqli_select_db($idcnx,$bd) or die ("hello word");
 
 ?>
